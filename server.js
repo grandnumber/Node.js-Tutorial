89cfcbf342a620c9416c46c84ad84becf062cec1
grandnumber <thegrandnumber@gmail.com>
2016-03-28 11:11:29 -0700

    add basics of server call

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var http = require("http");
+// require http module (ships w node) & make available with var http
 
 http.createServer(function(request, response){
   response.writeHead(200,{"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
 }).listen(8888);
+
+// pass the createServer an anonymous function - i.e. can refactor to:
+    // var http = require("http");
+    // 
+    // function onRequest(request, response){
+    //   response.writeHead(200, {"Content-Type": "text/plain"});
+    //   response.write("Hello World");
+    //   response.end();
+    // }
+    //
+    // http.createSever(onRequest).listen(888);
